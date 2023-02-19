98c66325309b04a4fb0579e2da5ee5f74abc5293
K-eli-wtech <kewhite3@my.waketech.edu>
2023-02-19 17:57:34 -0500

    True or false

--- arcadia/src/App.js
+++ arcadia/src/App.js
@@ -9,14 +9,20 @@ app.get('/button-click', async (req, res) => {
     // Get the game names from the query parameters
     const { game1, game2, game3, game4, game5 } = req.query;
     
+    // Check if the data exists in the database
+    const [result] = await dbC.pool.execute("select * from Users where name IN (?, ?, ?, ?, ?)", [game1, game2, game3, game4, game5]);
+    const dataExists = result.length > 0;
+    console.log(`Data exists: ${dataExists}`);
+    
     // Query the database for recommended games based on the input
-    const [rows] = await dbC.pool.execute("select * from Users where game1 ='Tarkov' and game2 ='Hogwarts Legacy' and game3 = 'Fallout 4' and game4 = 'Rust'and game5 = 'CSGO';", [game1, game2, game3, game4, game5]);
+    const [rows] = await dbC.pool.execute("select * from Users where name IN (?, ?, ?, ?, ?)", [game1, game2, game3, game4, game5]);
     const recommendedGames = rows.map(row => row.name);
     
     // Send the recommended games as the response
     res.send(`Recommended games: ${recommendedGames.join(', ')}`);
   } catch (error) {
     console.error(error);
+    console.log();
     res.status(500).send('Internal server error');
   }
 });
