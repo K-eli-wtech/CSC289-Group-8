df7a9154c5c85ea5b6ac64877e96cb3f33d53125
K-eli-wtech <kewhite3@my.waketech.edu>
2023-02-19 21:58:19 -0500

    Modifications

---
+++ arcadia/src/App.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dbC = require('./dbConfig');
+
+const app = express();
+const port = 3000;
+
+app.get('/service/button-click', async (req, res) => {
+    try {
+      dbC.dbInfo.getConnection(function(err){
+        if (err) throw err;
+        dbC.dbInfo.query("Select * from Users", function(err, results, fields) {
+          if (err) throw err;
+          res.json(results);
+        });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
