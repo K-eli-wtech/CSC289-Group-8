d020305f43222abb46fc357acbeacbf8b4391e9b
K-eli-wtech <kewhite3@my.waketech.edu>
2023-03-02 17:27:25 -0500

    Login and registration connected to database

--- client/public/password.js
+++ client/public/password.js
@@ -1,20 +1,33 @@
-class Password {
-    constructor(passwordDiv) {
-        this.passwordDiv = document.querySelector(passwordDiv)
-        this.passwordField = this.passwordDiv.querySelector("input")
-        this.toggle = this.passwordDiv.querySelector("button")
-        console.log(this.toggle);
-        this.toggle.addEventListener("click", this.showHide.bind(this, this.passwordField, this.toggle));
+/* eslint-disable no-unused-vars */
+function checkPass(){
+  let password = document.getElementById("pass").value;
+  let confirm = document.getElementById("verify").value;
+  let button = document.getElementById("register-button");
+  let message = document.getElementById("output");
 
+  if(password.length !== 0){
+    if(password === confirm){
+        message.textContent = ""
+        button.type = "submit"
+    } else {
+      message.textContent = "Passwords don't match"
+      message.style.color = '#ff4d4d'
     }
-    showHide(passwordField, toggle) {
-        if (passwordField.type === "password") {
-          passwordField.type = "text";
-          toggle.innerHTML = '<ion-icon name="eye-off-outline"></ion-icon>';
-        } else {
-          passwordField.type = "password";
-          toggle.innerHTML = '<ion-icon name="eye-outline"></ion-icon>';
-        }
-      }
-      
+  } else {
+    message.textContent = "Password can't be empty"
+      message.style.color = '#ff4d4d'
+  }
+}
+
+function showPass(pass, icon){
+  let password = document.getElementById(pass);
+  let eye = document.getElementById(icon);
+
+  if(password.type === "password"){
+    password.type = "text"
+    eye.name = "eye-off-outline"
+  } else {
+    password.type = "password"
+    eye.name = "eye-outline"
+  }
 }
