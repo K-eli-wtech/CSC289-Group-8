ee2c356b6483e570a52c46ee1adc53b8d5f6c6fe
K-eli-wtech <kewhite3@my.waketech.edu>
2023-03-23 16:48:16 -0400

    Main page Style updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState} from "react"
+import React, { useState } from "react"
 import './App.css';
-import Card from './Components/Card'
 
 class App extends React.Component {
   state = {
@@ -18,18 +17,6 @@ class App extends React.Component {
       <h1>Hello {this.state.name}!</h1>
     )
   }
-}
-
-function App() {
-  const [username, setUsername] = useState('Your Username');
-  const [game_review_title_1, setGameReviewTitle1] = useState('Review Title');
-  const [game_review_1, setGameReview1] = useState('Your Review');
-
-  return (
-    <div className='App'>
-      <Card username={username} game_review_title_1={game_review_title_1} game_review_1={game_review_1}/>
-    </div>
-  );
 }
 
 export default App
