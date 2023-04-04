47f91a06f81e8e5626b7dc448b9c8fa0f13fa23a
K-eli-wtech <kewhite3@my.waketech.edu>
2023-04-04 16:51:38 -0400

    Working profile/login session

--- client/public/js/profile-redirect.js
+++ client/public/js/profile-redirect.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-unused-vars */
 function redirectToProfileOrLogin() {
-    fetch('http://localhost:3000/pages/check-login')
-      .then((response) => {
-        if (response.status === 200) {
-          window.location.href = 'profile.html';
-        } else {
-          window.location.href = 'login.html';
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+  fetch('http://localhost:3000/pages/check-login')
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = 'profile.html';
+      } else {
         window.location.href = 'login.html';
-      });
-  }
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      window.location.href = 'login.html';
+    });
+}
--- client/server/routes/form.js
+++ client/server/routes/form.js
@@ -65,13 +65,28 @@ formRouter.get('/login', async (req, res) => {
 });
 
 
-// In form.js or another suitable file
-formRouter.get('/is-logged-in', (req, res) => {
+// Handles logged in session data
+formRouter.get('/check-login', (req, res) => {
   if (req.session.loggedIn) {
     res.status(200).json({ loggedIn: true });
   } else {
-    res.status(200).json({ loggedIn: false });
+    res.status(401).json({ loggedIn: false });
   }
+});
+
+
+// Handles logging out
+formRouter.get('/logout', (req, res) => {
+  console.log("Before logout:", req.session);
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error logging out');
+    } else {
+      console.log("After logout:", req.session);
+      res.status(200).send('Logged out');
+    }
+  });
 });
 
 
