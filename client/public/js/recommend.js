afeec91d28a1f1223f5fd21b76eddbc1e677f5a3
K-eli-wtech <kewhite3@my.waketech.edu>
2023-04-05 18:38:25 -0400

    Can search multiple times in recommender now

--- client/public/js/recommend.js
+++ client/public/js/recommend.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 const selectedGenres = new Set();
 const genresGrid = document.getElementById("genres-grid");
 const continueButton = document.getElementById("continue-button");
 const resultsParagraph = document.getElementById("result");
+const container = document.getElementsByClassName("container")[0];
 
 genresGrid.addEventListener("click", (event) => {
     if (event.target.closest(".genre-card")) {
@@ -24,38 +26,43 @@ continueButton.addEventListener("click", async () => {
         resultsParagraph.textContent = "Please select only 3 genres.";
         return;
     }
+    if (continueButton.textContent === "Continue") {
+        continueButton.textContent = "Again";
 
-    // Hide genre selection container
-    document.getElementById("genre-selection-container").style.display = "none";
+        // Hide genre selection container
+        document.getElementById("genre-selection-container").style.display = "none";
+        container.style.width = "90%";
 
-    // Show results container
-    const resultsContainer = document.getElementById("results-container");
-    resultsContainer.style.display = "flex";
+        // Show results container
+        const resultsContainer = document.getElementById("results-container");
+        resultsContainer.style.display = "flex";
 
-    // Create containers for each genre
-    const genreContainers = Array.from(selectedGenres).map((genreId) => {
-        const genre = gameGenres.find((g) => g.id === parseInt(genreId));
-        const genreContainer = document.createElement("div");
-        genreContainer.className = "genre-results-container";
+        // Create containers for each genre
+        const genreContainers = Array.from(selectedGenres).map((genreId) => {
+            const genre = gameGenres.find((g) => g.id === parseInt(genreId));
+            const genreContainer = document.createElement("div");
+            genreContainer.className = "genre-results-container";
 
-        const genreTitle = document.createElement("h2");
-        genreTitle.textContent = genre.name;
-        genreContainer.appendChild(genreTitle);
+            const genreTitle = document.createElement("h2");
+            genreTitle.textContent = genre.name;
+            genreContainer.appendChild(genreTitle);
 
-        const genreGamesContainer = document.createElement("div");
-        genreGamesContainer.className = "genre-games-container";
-        genreContainer.appendChild(genreGamesContainer);
+            const genreGamesContainer = document.createElement("div");
+            genreGamesContainer.className = "genre-games-container";
+            genreContainer.appendChild(genreGamesContainer);
 
-        resultsContainer.appendChild(genreContainer);
+            resultsContainer.appendChild(genreContainer);
 
-        return { genre, genreGamesContainer };
-    });
+            return { genre, genreGamesContainer };
+        });
 
-    // Fetch recommended games concurrently
-    await Promise.all(
-        genreContainers.map(({ genre, genreGamesContainer }) =>
-            genreGames(genre, genreGamesContainer, 6, "mini")
-        )
-    );
+        // Fetch recommended games concurrently
+        await Promise.all(
+            genreContainers.map(({ genre, genreGamesContainer }) =>
+                genreGames(genre, genreGamesContainer, 6, "mini")
+            )
+        );
+    } else {
+        location.reload();
+    }
 });
-
